8569d8ad64fdbdf32f24e23b75f228554e79e754
luizfelipew <luiz.f.campos@partner.bmw.com.br>
2023-11-24 18:31:59 -0300

    prompt engineering

--- python_chat/categorizador-aula-2/categorizador.py
+++ python_chat/categorizador-aula-2/categorizador.py
@@ -4,18 +4,33 @@ import dotenv
 
 dotenv.load_dotenv()
 
+# Provavelmente esse OpenAI() pega todas as variaveis de sistemas necessárias
+#  por isso não usamos openai.api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI()
+
+prompt_sistema = """"
+Você é um categorizador de produtos.
+Você deve escolher uma categoria da lista abaixo:
+##### Lista de categorias válidas
+Beleza
+Entreterimento
+Esportes
+Outros
+##### Exemplo
+bola de tênis
+Esportes
+"""
 
 resposta = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
     {
       "role": "system",
-      "content": "Você é um categorizador de produtos."
+      "content": prompt_sistema
     },
     {
       "role": "user",
-      "content": "escova de dentes"
+      "content": "bola de tênis de mesa"
     }
   ],
   temperature=1,
