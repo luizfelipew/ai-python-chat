c32948254caf4a7d246634c340571ca7863348f3
luizfelipew <luiz.f.campos@partner.bmw.com.br>
2023-11-29 21:00:34 -0300

    token and tiktoken counter

---
+++ python_chat/tokens-aula-3/contagem_de_tokens.py
@@ -0,0 +1,9 @@
+# from tiktoken import TikToken
+import tiktoken
+
+codificador = tiktoken.encoding_for_model("gpt-3.5-turbo")
+lista_de_tokens = codificador.encode("Você é um categorizador de produtos.")
+print(lista_de_tokens)
+print(len(lista_de_tokens))
+custo_de_entrada = (len(lista_de_tokens)/ 1000) * 0.0015
+print(f"Custo: {custo_de_entrada}")
